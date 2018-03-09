00d430e9d8ee7973a07323e7201ef2e951eaf2d8
xjhnear <xiajiahui@jucaicat.com>
2018-03-09 16:03:48 +0800

    fix
    Signed-off-by: xjhnear <xiajiahui@jucaicat.com>

---
+++ backend/views/return/config.php
@@ -0,0 +1,80 @@
+<?php
+
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+use common\core\ActiveForm;
+use common\helpers\ArrayHelper;
+use backend\assets\AppAsset;
+//use backend\models\Train;
+//use backend\models\Shop;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\Menu */
+/* @var $form ActiveForm */
+
+/* ===========================以下为本页配置信息================================= */
+/* 页面基本属性 */
+$this->title = '自动返还';
+$this->params['title_sub'] = '';  // 在\yii\base\View中有$params这个可以在视图模板中共享的参数
+
+?>
+
+<div class="portlet light bordered">
+    <div class="portlet-title">
+        <div class="caption font-red-sunglo">
+            <i class="icon-settings font-red-sunglo"></i>
+            <span class="caption-subject bold uppercase"> 返还设置</span>
+        </div>
+    </div>
+    <div class="portlet-body form">
+        <!-- 这里注意了，不能使用pjax，因为第三方库中有行内js，会导致js加载失败 -->
+        <!-- BEGIN FORM-->
+        <?php $form = ActiveForm::begin([
+            'options'=>[
+                'class'=>"form-aaa"
+            ]
+        ]); ?>
+
+        <div class="form-group field-admin-status">
+            <div><label class="" for="admin-status">开关设置</label></div>
+            <input type="hidden" name="Config[value]" value="">
+            <label class="mt-radio mt-radio-outline" style="padding-right:20px;margin-bottom:5px;"><input type="radio" name="Config[value]" value="1" <?php if($model['value'] == 1) { ?>checked=""<?php } ?>><span></span> 开启</label>
+            <label class="mt-radio mt-radio-outline" style="padding-right:20px;margin-bottom:5px;"><input type="radio" name="Config[value]" value="0" <?php if($model['value'] == 0) { ?>checked=""<?php } ?>><span></span> 关闭</label><span class="help-block"></span>
+        </div>
+
+        <div class="form-actions">
+            <?= Html::submitButton('<i class="icon-ok"></i> 保存', ['id' => 'sub','class' => 'btn blue ajax-post','target-form'=>'form-aaa']) ?>
+        </div>
+
+        <?php ActiveForm::end(); ?>
+
+        <!-- END FORM-->
+    </div>
+</div>
+
+<?php
+AppAsset::register($this);
+//只在该视图中使用非全局的jui
+AppAsset::addScript($this,'static/js/ajaxfileupload.js');
+?>
+
+<!-- 定义数据块 -->
+<?php $this->beginBlock('test'); ?>
+
+$(function() {
+/* 子导航高亮 */
+highlight_subnav('return/config');
+});
+function sures()
+{
+if(confirm('确定继续？'))
+{
+return true;
+}else{
+return false;
+}
+
+<?php $this->endBlock() ?>
+<!-- 将数据块 注入到视图中的某个位置 -->
+<?php $this->registerJs($this->blocks['test'], \yii\web\View::POS_END); ?>
--- backend/views/return/index.php
+++ backend/views/return/index.php
@@ -1,153 +1,105 @@
 <?php
 
-use yii\helpers\Html;
-use yii\grid\GridView;
 
-/* @var $model common\modelsgii\Message */
-/* @var $dataProvider yii\data\ActiveDataProvider  */
-/* @var $searchModel backend\models\search\MessageSearch */
+use yii\helpers\Html;
+use yii\helpers\Url;
+use common\core\ActiveForm;
+use common\helpers\ArrayHelper;
+use backend\assets\AppAsset;
+//use backend\models\Train;
+//use backend\models\Shop;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\Menu */
+/* @var $form ActiveForm */
 
 /* ===========================以下为本页配置信息================================= */
 /* 页面基本属性 */
-$this->title = '短信管理';
+$this->title = '手动返还';
 $this->params['title_sub'] = '';  // 在\yii\base\View中有$params这个可以在视图模板中共享的参数
-
-/* 先要注册表格所须的资源 */
-\backend\assets\TablesAsset::register($this);
-
-$columns = [
-    [
-        'class' => \common\core\CheckboxColumn::className(),
-        'name'  => 'id',
-        'options' => ['width' => '20px;'],
-        'checkboxOptions' => function ($model, $key, $index, $column) {
-            return ['value' => $key,'label'=>'<span></span>','labelOptions'=>['class' =>'mt-checkbox mt-checkbox-outline','style'=>'padding-left:19px;']];
-        }
-    ],
-    [
-        'header' => 'ID',
-        'value' => 'message_id',
-        'options' => ['width' => '50px;']
-    ],
-    [
-        'header' => '批次号',
-        'value' => 'message_code',
-        'options' => ['width' => '50px;'],
-    ],
-    [
-        'header' => '发送时间',
-        'value' => 'send_time',
-        'format' => ['date', 'php:Y-m-d H:i:s'],
-        'options' => ['width' => '150px;'],
-    ],
-    [
-        'header' => '短信内容',
-        'value' => 'content',
-        'options' => ['width' => '150px;'],
-    ],
-    [
-        'header' => '数量',
-        'value' => 'count',
-        'options' => ['width' => '50px;']
-    ],
-    [
-        'label' => '状态',
-        'value' => 'status',
-        'options' => ['width' => '80px;'],
-        'content' => function($model){
-            return Yii::$app->params['send_status'][$model['status']];
-        },
-
-    ],
-    [
-        'label' => '创建时间',
-        'value' => 'create_time',
-        'options' => ['width' => '150px;'],
-        'format' =>  ['date', 'php:Y-m-d H:i:s']
-    ],
-    [
-        'class' => 'yii\grid\ActionColumn',
-        'header' => '操作',
-        'template' => '{view}',
-        'options' => ['width' => '100px;'],
-        'buttons' => [
-            'view' => function ($url, $model, $key) {
-                return Html::a('<i class="fa fa-eye"></i>', ['detail', 'pid'=>$key], [
-                    'title' => Yii::t('app', '详细信息'),
-                    'class' => 'btn btn-xs blue'
-                ]);
-            },
-        ],
-        'headerOptions' => [],
-    ],
-];
 
 ?>
 
-<div class="portlet light portlet-fit portlet-datatable bordered">
+<div class="portlet light bordered">
     <div class="portlet-title">
-        <div class="caption">
-            <i class="icon-settings font-dark"></i>
-            <span class="caption-subject font-dark sbold uppercase">短信列表</span>
-        </div>
-        <div class="actions">
-            <div class="btn-group btn-group-devided">
-                <?=Html::a('清空搜索 <i class="fa fa-times"></i>',['message/index'],['class'=>'btn green','style'=>'margin-right:10px;'])?>
-            </div>
-            <div class="btn-group">
-                <button class="btn blue btn-sm dropdown-toggle" type="button" data-toggle="dropdown">
-                    工具箱
-                    <i class="fa fa-angle-down"></i>
-                </button>
-                <ul class="dropdown-menu pull-right" role="menu">
-                    <li><a href="javascript:;"><i class="fa fa-pencil"></i> 导出Excel </a></li>
-                    <li class="divider"> </li>
-                    <li><a href="javascript:;"> 其他 </a></li>
-                </ul>
-            </div>
+        <div class="caption font-red-sunglo">
+            <i class="icon-settings font-red-sunglo"></i>
+            <span class="caption-subject bold uppercase"> 返还设置</span>
         </div>
     </div>
-    <div class="portlet-body">
-        <?php \yii\widgets\Pjax::begin(['options'=>['id'=>'pjax-container']]); ?>
-        <div>
-            <?php echo $this->render('_search', ['model' => $searchModel]); ?> <!-- 条件搜索-->
+    <div class="portlet-body form">
+        <!-- 这里注意了，不能使用pjax，因为第三方库中有行内js，会导致js加载失败 -->
+        <!-- BEGIN FORM-->
+        <?php $form = ActiveForm::begin([
+            'options'=>[
+                'class'=>"form-aaa"
+            ]
+        ]); ?>
+
+        <div class="form-group field-message-phonenumbers">
+            <div><label class="" for="message-phonenumbers">返还信息</label><span class="help-inline"></span></div><span class="help-block"></span>
+            <input type="hidden" name="Config[model_json]" value='<?=$model_json ?>'>
+            <table class="table table-striped table-bordered table-hover table-checkable order-column dataTable no-footer">
+                <colgroup>
+                    <col width="100px;">
+                    <col width="150px;">
+                    <col width="100px;">
+                    <col width="150px;">
+                </colgroup>
+                <thead>
+                <tr><th>ID</th><th>批次号</th><th>用户ID</th><th>待返还数量</th><th>发送时间</th></tr>
+                </thead>
+                <tbody>
+                <?php if(count($model)> 0) { ?>
+                <?php foreach ($model as $item) { ?>
+                    <tr>
+                        <td style="vertical-align: middle;"><?=$item['message_id'] ?></td>
+                        <td style="vertical-align: middle;"><?=$item['message_code'] ?></td>
+                        <td style="vertical-align: middle;"><?=$item['create_uid'] ?></td>
+                        <td style="vertical-align: middle;"><?=$item['balance'] ?></td>
+                        <td style="vertical-align: middle;"><?=$item['send_time'] ?></td>
+                    </tr>
+                <?php } ?>
+                <?php }else{ ?>
+                    <tr><td colspan="5"><div class="empty">没有找到数据。</div></td></tr>
+                <?php } ?>
+                </tbody>
+            </table>
         </div>
-        <div class="table-container">
-            <form class="ids">
-            <?= GridView::widget([
-                'dataProvider' => $dataProvider, // 列表数据
-                'filterModel' => $searchModel, // 搜索模型
-                'options' => ['class' => 'grid-view table-scrollable'],
-                /* 表格配置 */
-                'tableOptions' => ['class' => 'table table-striped table-bordered table-hover table-checkable order-column dataTable no-footer'],
-                /* 重新排版 摘要、表格、分页 */
-                'layout' => '{items}<div class=""><div class="col-md-5 col-sm-5">{summary}</div><div class="col-md-7 col-sm-7"><div class="dataTables_paginate paging_bootstrap_full_number" style="text-align:right;">{pager}</div></div></div>',
-                /* 配置摘要 */
-                'summaryOptions' => ['class' => 'pagination'],
-                /* 配置分页样式 */
-                'pager' => [
-                    'options' => ['class'=>'pagination','style'=>'visibility: visible;'],
-                    'nextPageLabel' => '>>',
-                    'prevPageLabel' => '<<',
-                    'firstPageLabel' => '首页',
-                    'lastPageLabel' => '尾页',
-                    'maxButtonCount'=> 5
-                ],
-                /* 定义列表格式 */
-                'columns' => $columns,
-            ]); ?>
-            </form>
+
+        <div class="form-actions">
+            <?= Html::submitButton('<i class="icon-ok"></i> 确认返还', ['id' => 'sub','class' => 'btn blue ajax-post','target-form'=>'form-aaa']) ?>
         </div>
-        <?php \yii\widgets\Pjax::end(); ?>
+
+        <?php ActiveForm::end(); ?>
+
+        <!-- END FORM-->
     </div>
 </div>
 
+<?php
+AppAsset::register($this);
+//只在该视图中使用非全局的jui
+AppAsset::addScript($this,'static/js/ajaxfileupload.js');
+?>
 
 <!-- 定义数据块 -->
 <?php $this->beginBlock('test'); ?>
-jQuery(document).ready(function() {
-    highlight_subnav('message/index'); //子导航高亮
+
+$(function() {
+/* 子导航高亮 */
+highlight_subnav('return/index');
+
+$('#sub').click(function(){
+if(confirm('确认返还？'))
+{
+return true;
+}else{
+return false;
+}
 });
+});
+
 <?php $this->endBlock() ?>
 <!-- 将数据块 注入到视图中的某个位置 -->
 <?php $this->registerJs($this->blocks['test'], \yii\web\View::POS_END); ?>
